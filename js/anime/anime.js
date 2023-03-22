c26a60e1a8f5cc139e29b1d38502f73223cdaeb5
Redzone <naveen@internetbusinesssolutionsindia.com>
2023-03-22 11:23:55 +0530

    parellex working

--- js/anime/anime.js
+++ js/anime/anime.js
@@ -10,10 +10,8 @@ function AnimateBox(e) {
         Math.floor(100 + Math.random() * -999),
         Math.floor(100 + Math.random() * 999),
       ]), 
-
-      
-      
     });
+
     anime({
       targets: e,
       translateX: 0,
@@ -23,37 +21,40 @@ function AnimateBox(e) {
       easing: "easeOutExpo",
     });
 }
-
 const p_1 = document.querySelectorAll(".statBox1 .el");
-
 const p_2 = document.querySelectorAll(".statBox2 .el");
-
 const p_3 = document.querySelectorAll(".statBox3 .el");
-
 const p_4 = document.querySelectorAll(".statBox4 .el");
 
 
-const x = 0;
+// Get the element you want to animate
+// jQuery document ready
+$(document).ready(function() {
+  var myElement = document.getElementById('statistics');
+  console.log("myElement", myElement); 
+  var myElement = document.querySelector('.statistics');
+  var observer = new IntersectionObserver(function(entries) {
+    console.log("animation runs 0 takes"); 
+    if (entries[0].isIntersecting) {
+      TakesEff();
+      console.log("animation runs 1 takes"); 
+    }
+  }, { threshold: 0.5 });
+  observer.observe(myElement);
+  observer.unobserve(myElement);
+});
 
-// $(document).scroll(function(){
-//   console.log('hhhhhhhhhhhhhhh');
-//   if ($("body").hasClass("statistics-active") && x == 0) {
 
-  // $(".statBox").css('opacity', 1);
-//     x++;
-//   }
-// })
- 
-    AnimateBox(p_1, ".statBox1  .el");
-    anime({
-      targets: ".statBox1 .el",
-      translateX: 0,
-      opacity: 1,
-      translateY: 0,
-      duration: 1500,
-      easing: "easeOutExpo",
-    });
-    AnimateBox(p_2, ".statBox2 .el");   
+
+function TakesEff () { 
+
+    // animation boxex effects on function hits 
+    AnimateBox(p_1);
+    AnimateBox(p_2);   
+    AnimateBox(p_3);
+    AnimateBox(p_4);
+    
+    // animation text effects on function hits
     anime({
       targets: ".el-text2",
       bottom: "0px",
@@ -61,7 +62,6 @@ const x = 0;
       easing: "easeInOutQuad",
       duration: 1500,
     });
-    AnimateBox(p_3, ".statBox3 .el");
     anime({
       targets: ".el-text1",
       left: "0px",
@@ -69,7 +69,6 @@ const x = 0;
       easing: "easeInOutQuad",
       duration: 1500,
     });
-    AnimateBox(p_4, ".statBox4 .el");
     anime({
       targets: ".el-text3",
       right: "0px",
@@ -77,4 +76,9 @@ const x = 0;
       easing: "easeInOutQuad",
       duration: 1500,
     });
+
+} 
+
+ 
+
     
